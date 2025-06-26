445cedc2599bfb4af231088d85efde45a3428c9b
Juando26030 <juando02603@gmail.com>
2025-06-26 17:50:32 -0500

    menu funcional para navegar y con estilos

--- segar-frontend/src/app/app.routes.ts
+++ segar-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MenuLayoutComponent } from './layout/menu-layout/menu-layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TramitesComponent } from './pages/tramites/tramites.component';
+import { NuevoTramiteComponent } from './pages/nuevo-tramite/nuevo-tramite.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MenuLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'tramites', component: TramitesComponent },
+      { path: 'nuevo', component: NuevoTramiteComponent },
+    ]
+  },
+  { path: '**', redirectTo: 'dashboard' }
+];
---
+++ segar-frontend/src/app/pages/tramites/tramites.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tramites',
+  imports: [],
+  templateUrl: './tramites.component.html',
+  styleUrl: './tramites.component.css'
+})
+export class TramitesComponent {
+
+}
