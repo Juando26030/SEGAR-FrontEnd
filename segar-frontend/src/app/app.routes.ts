a51d81ad5c61ffd16376aa6c5b67741ba40ef3fc
Juando26030 <juando02603@gmail.com>
2025-06-26 20:11:38 -0500

    integración con tailwind y panel principal listo  (quemado)

--- segar-frontend/src/app/app.routes.ts
+++ segar-frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { MenuLayoutComponent } from './layout/menu-layout/menu-layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PanelPrincipalComponent } from './pages/panel-principal/panel-principal.component';
 import { TramitesComponent } from './pages/tramites/tramites.component';
 import { NuevoTramiteComponent } from './pages/nuevo-tramite/nuevo-tramite.component';
 
@@ -9,11 +9,11 @@ export const routes: Routes = [
     path: '',
     component: MenuLayoutComponent,
     children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: 'panel', pathMatch: 'full' },
+      { path: 'panel', component: PanelPrincipalComponent },
       { path: 'tramites', component: TramitesComponent },
-      { path: 'nuevo', component: NuevoTramiteComponent },
+      { path: 'nuevo', component: NuevoTramiteComponent }
     ]
   },
-  { path: '**', redirectTo: 'dashboard' }
+  { path: '**', redirectTo: 'panel' }
 ];
