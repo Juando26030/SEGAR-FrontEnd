f94cffec0578df66ed3d5d65779f378035781c4a
Juando26030 <juando02603@gmail.com>
2025-06-30 21:02:59 -0500

    paso 2 componente subido a git

---
+++ segar-frontend/src/app/tramites/registro/registro-paso-dos/registro-paso-dos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-paso-dos',
+  imports: [],
+  templateUrl: './registro-paso-dos.component.html',
+  styleUrl: './registro-paso-dos.component.css'
+})
+export class RegistroPasoDosComponent {
+
+}
