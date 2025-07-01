6bec71abfea60f183e685b32ca0fe554e0de59cc
Juando26030 <juando02603@gmail.com>
2025-06-30 19:56:06 -0500

    añadir componente de registro paso uno al git

---
+++ segar-frontend/src/app/tramites/registro/registro-paso-uno/registro-paso-uno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-paso-uno',
+  imports: [],
+  templateUrl: './registro-paso-uno.component.html',
+  styleUrl: './registro-paso-uno.component.css'
+})
+export class RegistroPasoUnoComponent {
+
+}
