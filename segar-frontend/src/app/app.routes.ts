5f42259cc59179b35895117fb426e22a67afc26f
Juando26030 <juando02603@gmail.com>
2025-06-30 19:55:05 -0500

    pantalla de nuevo tramite con las 3 posibles opciones

--- segar-frontend/src/app/app.routes.ts
+++ segar-frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { MenuLayoutComponent } from './layout/menu-layout/menu-layout.component';
 import { PanelPrincipalComponent } from './pages/panel-principal/panel-principal.component';
-import { TramitesComponent } from './pages/tramites/tramites.component';
 import { NuevoTramiteComponent } from './pages/nuevo-tramite/nuevo-tramite.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CalendarioComponent } from './pages/calendario/calendario.component';
@@ -17,7 +16,6 @@ export const routes: Routes = [
       { path: '', redirectTo: 'panel', pathMatch: 'full' },
       { path: 'panel', component: PanelPrincipalComponent },
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'tramites', component: TramitesComponent },
       { path: 'calendario', component: CalendarioComponent },
       { path: 'nuevo', component: NuevoTramiteComponent }
     ]
