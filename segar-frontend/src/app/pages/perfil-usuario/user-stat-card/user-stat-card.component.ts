59328448fb0cd89ae46cda9c0d29773c355f5bbe
johny2004 <jonathanjurados@gmail.com>
2025-07-01 10:21:03 -0500

    Componentes de editar perfil y ver perfil

---
+++ segar-frontend/src/app/pages/perfil-usuario/user-stat-card/user-stat-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-stat-card',
+  imports: [],
+  templateUrl: './user-stat-card.component.html',
+  styleUrl: './user-stat-card.component.css'
+})
+export class UserStatCardComponent {
+
+}
