9e55f18e106c779fdae74c50818a7a2007998a59
Juando26030 <juando02603@gmail.com>
2025-06-30 23:44:12 -0500

    subido al git componente de generador de documentos

---
+++ segar-frontend/src/app/tramites/generador-documentos/generador-documentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generador-documentos',
+  imports: [],
+  templateUrl: './generador-documentos.component.html',
+  styleUrl: './generador-documentos.component.css'
+})
+export class GeneradorDocumentosComponent {
+
+}
