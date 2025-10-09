185528147bc8db284ce07badb18ce1afef7d59b7
Lucas R <lucasrivera6021@gmail.com>
2025-10-08 19:07:39 -0500

    Version inicial de busqueda global

---
+++ segar-frontend/src/app/pages/busqueda-global/busqueda-global.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
+
+@Component({
+  selector: 'app-busqueda-global',
+  imports: [
+
+  ],
+  templateUrl: './busqueda-global.component.html',
+  styleUrl: './busqueda-global.component.css'
+})
+export class BusquedaGlobalComponent {
+
+}
